9be0ef4706aca4179ac2135fdb51a1eb82a8a492
pratham <pratham@interbitsolutions.com>
2023-04-04 21:06:10 +0530

    captcha issue fixed

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-// const puppeteer = require("puppeteer");
-
-// const res = async () => {
-//   const browser = await puppeteer.launch({
-//     executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
-//   });
-
-//   const page = await browser.newPage();
-//   await page.goto("https://review-blogger.com/signup?redirect=");
-
-//   // Fill in the username field
-//   await page.type('input[name="usr"]', "pratham876");
-
-//   // Fill in the password field
-//   await page.type('input[name="pass"]', "pass@654");
-
-//   // Fill in the email field
-//   await page.type('input[name="email"]', "pratham@gmail.com");
-
-//   // Submit the form
-//   await page.click('input[type="submit"]');
-//   await page.screenshot({ path: "ss.png" });
-//   await browser.close();
-// };
-
-// res();
-
 const puppeteerExtra = require("puppeteer-extra");
+const axios = require("axios");
+const fs = require("fs");
 
 const RecaptchaPlugin = require("puppeteer-extra-plugin-recaptcha");
+
 puppeteerExtra.use(
   RecaptchaPlugin({
     provider: {
       id: "2captcha",
-      token: "1e7c2b15ff017ec0c4df9426562a997b", // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+      token: process.env.KEY, // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
     },
     visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
-    solveScoreBased:true
   })
 );
 
@@ -49,96 +24,97 @@ puppeteerExtra
     const page = await browser.newPage();
     await page.goto("https://review-blogger.com/signup?redirect=");
 
-    await page.type('input[name="usr"]', "pratham876");
+    await page.type('input[name="usr"]', "pratham111");
 
     await page.type('input[name="pass"]', "pass@6745");
 
     await page.type('input[name="email"]', "pratham@interbitsolutions.com");
+
     await page.screenshot({ path: "response.png", fullPage: true });
 
     await page.click(`#captcha_window > div > label > div.captcha_checkbox`);
 
-    // let { captchas, filtered, error } = await page.findRecaptchas()
+    await page.waitForTimeout(3000);
 
-    // console.log(captchas)
+    // Wait for the element to appear on the page
+    await page.waitForSelector("#imgs-window");
 
-    await page.waitForTimeout(2000);
-    await page
-      .solveRecaptchas()
-      // .then((res) => {
-      //   console.log("solved");
-      //   console.log(res);
-      //    page.click(`#captcha_verify_button`).then((res1)=>{
-      //     console.log("i am response1" , res1)
-      //      page.screenshot({ path: "response1.png", fullPage: true });
-      //      Promise.all([
-      //       page.waitForNavigation(),
-      //       page.click(
-      //         `#identity > footer.access-form__footer.signup__footer.hide_if_succesfuly > input.access-form__submit.signup-btn.submit-btn`
-      //       ),
-      //     ]);
-      //   });
-       
-      // })
-      // .catch((err) => {
-      //   console.log("failed");
-      //   console.log(err);
-      // });
+    // Get the dimensions of the element
+    const divElement = await page.$("#imgs-window");
+    const dimensions = await divElement.boundingBox();
 
-    
+    // Capture a screenshot of only the element
+    await page.screenshot({
+      path: "response4.png",
+      clip: dimensions,
+    });
 
-    // await browser.waitForTarget(() => false);
-    await browser.close();
+    const image = fs.readFileSync("response4.png");
+
+    // Convert the image data to Base64 encoding
+    const base64Image = Buffer.from(image).toString("base64");
+
+    let data = new FormData();
+    data.append("method", "base64");
+    data.append("recaptcha", "1");
+    data.append("canvas", "0");
+    data.append("key", "28e40c739f9bfab7cb86b88f59b9a874");
+    data.append("body", base64Image);
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://2captcha.com/in.php",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        let id = response.data.split("|")[1];
+
+        const intervel = setInterval(() => {
+          let config = {
+            method: "get",
+            maxBodyLength: Infinity,
+            url: `http://2captcha.com/res.php?key=28e40c739f9bfab7cb86b88f59b9a874&action=get&id=${id}`,
+          };
+
+          axios
+            .request(config)
+            .then(async (response) => {
+              if (response.data != "CAPCHA_NOT_READY") {
+                let results = response.data
+                  .split("|")[1]
+                  .split(":")[1]
+                  .split("/");
+
+                for (element of results) {
+                  await page.waitForSelector(`#captcha${element}`);
+                  await page.click(`#captcha${element}`);
+                }
+
+                await page.click(`#captcha_verify_button`).then((res1) => {
+                  clearInterval(intervel);
+                  page.click(
+                    `#identity > footer.access-form__footer.signup__footer.hide_if_succesfuly > input.access-form__submit.signup-btn.submit-btn`
+                  );
+                });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }, 5000); // Repeat every 5 seconds (5000 milliseconds)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+  .catch((err) => {
+    console.log("errdata");
+    console.log(err);
   });
-
-// const puppeteerExtra = require("puppeteer-extra");
-// const RecaptchaPlugin = require("puppeteer-extra-plugin-recaptcha");
-// const DeathByCaptcha = require("deathbycaptcha");
-// const { HttpClient } = require('deathbycaptcha');
-
-
-
-// puppeteerExtra.use(
-//   RecaptchaPlugin({
-//     provider: {
-//       id: "dbc",
-//       username: "ecpecp",
-//       password: "Ecp1122@@$$",
-//     },
-//     visualFeedback: true, // optional
-//   })
-// );
-
-// const res = async () => {
-//   const dbc = new DeathByCaptcha("ecpecp", "Ecp1122@@$$");
-
-//   const browser = await puppeteerExtra.launch({
-//     executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
-//     headless: false,
-//   });
-//   const page = await browser.newPage();
-
-//   const client = new HttpClient('ecpecp', 'Ecp1122@@$$');
-
-//   await page.goto("https://review-blogger.com/signup?redirect=");
-
-//   await page.click(`#captcha_window > div > label > div.captcha_checkbox`);
-//   await page.screenshot({ path: "response.png", fullPage: true });
-
-//   // solve reCAPTCHA challenge using DeathByCaptcha
-//   await page.solveRecaptchas({
-//     providerCallback: (token) => {
-//       return new Promise((resolve, reject) => {
-//         dbc.decode({ captcha: token }, (err, result) => {
-//           if (err) reject(err);
-//           resolve(result.text);
-//         });
-//       });
-//     },
-//   });
-//   await page.click(`#captcha_verify_button`);
-
-//   await page.screenshot({ path: "response1.png", fullPage: true });
-// };
-
-// res();
